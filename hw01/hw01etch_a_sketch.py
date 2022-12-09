4e3c6afe26fb8053b0af225ee55b1cf53f260f3b
rhit-RyanSu <73152221+rhit-RyanSu@users.noreply.github.com>
2022-12-09 12:09:20 -0500

    Add files via upload

---
+++ hw01/hw01etch_a_sketch.py
@@ -0,0 +1,89 @@
+#hw01 etch_a_sketch
+#12/7/2022
+#ECE434
+#Ryan Su
+#simple etch a sketch board
+#1. enter any size to create a square board
+#2. enter X and Y coordinates for initial pen location
+#3. start sketching with the display
+#4. action includes (1)up: move pen up a slot and draw
+#                   (2)down: move pen down a slot and draw
+#                   (3)left: move pen left a slot and draw
+#                   (4)right: move pen right a slot and draw
+#                   (5)erase: erase current slot, pen stays
+#                   (6)dump: dump the current drawing and start new page, pen stays
+#                   (7)quit: quit drawing
+#5. current pen location is shown under the sketch board after each action
+#
+
+# functions
+def print_grid(grid):
+    for row in grid:
+        print(' '.join(str(element) for element in row))
+
+def initialization(dimensions):
+    grid = [[]]
+    grid[0].append('   ')
+    for k in range(0, dimensions):
+        grid[0].append(k+1)
+        grid.append([k+1])
+        grid[k+1].append(':')
+        for i in range(0,dimensions):
+            grid[k+1].append(' ')
+    return grid
+
+#main
+dimensions = int(input("enter a size for the sketch grid: "))
+grid = initialization(dimensions)
+
+currX = int(input("enter a initial pen location(x): "))
+currY = int(input("enter a initial pen location(y): "))
+while((currX > dimensions) or (currY > dimensions)):
+    print("out of range, another try")
+    currX = int(input("enter a initial pen location(x): "))
+    currY = int(input("enter a initial pen location(y): "))
+
+offsetY = 1
+grid[currX][offsetY+currY] = 'x'
+print_grid(grid)
+print("Current Pen location: "+str(currX)+", "+str(currY))
+
+while(1 + 1 == 2):
+    action = str(raw_input("action? (up, down, left, right, erase, dump, quit): "))
+    while(action!="up" and action!="down" and action!="left" and action!="right" and action!="erase" and action!="quit" and action!="dump"):
+            action = str(raw_input("wrong input, another try (up, down, left, right, erase, dump, quit): "))
+    if(action == "up"):
+        currX = currX-1
+        currY = currY
+        grid[currX][offsetY+currY] = 'x'
+    elif(action == "down"):
+        currX = currX+1
+        currY = currY
+        grid[currX][offsetY+currY] = 'x'
+    elif(action == "left"):
+        currX = currX
+        currY = currY-1
+        grid[currX][offsetY+currY] = 'x'
+    elif(action == "right"):
+        currX = currX
+        currY = currY+1
+        grid[currX][offsetY+currY] = 'x'
+    elif(action == "erase"):
+        currX = currX
+        currY = currY
+        grid[currX][offsetY+currY] = ' '
+    elif(action == "dump"):
+        grid = initialization(dimensions)
+    elif(action == "quit"):
+        break
+
+    print_grid(grid)
+    print("Current Pen location: "+str(currX)+", "+str(currY))
+
+    
+
+
+
+
+
+
